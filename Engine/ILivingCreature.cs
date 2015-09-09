dd1327199bddc772b10414dd95bd3ce5982c5aaf
JohnnySlocum <johnny.slocum@icloud.com>
2015-09-09 15:32:55 -0400

    Added a constructor to the 'Location' class.

---
+++ Engine/ILivingCreature.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public interface ILivingCreature
+    {
+        int MaximumHitPoints { get; set; }
+        int CurrentHitPoints { get; set; }
+    }
+}
---
+++ SuperAdventureReadMe.txt
@@ -0,0 +1,1 @@
+//This is the read me file for the SuperAdventure type along tutorial.
